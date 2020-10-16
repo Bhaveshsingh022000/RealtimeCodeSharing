7c058161e3727e1da927dbb68cda8d628f4dbb0c
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-10-16 18:26:22 +0530

    added icons to sideDrawer

--- frontend/app/src/Components/UI/sideDrawer/sideDrawer.js
+++ frontend/app/src/Components/UI/sideDrawer/sideDrawer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShareAlt, faCode } from '@fortawesome/free-solid-svg-icons'
 import classes from './sideDrawer.module.css';
 
 const sideDrawer = (props) => {
     return (
         <div className={classes.SideDrawer}>
-            <button onClick={props.click}>s</button>
+            <button className={classes.ButtonIcon} onClick={props.click}><FontAwesomeIcon icon={faShareAlt} /></button>
+            <button className={classes.ButtonIcon} onClick={props.click}><FontAwesomeIcon icon={faCode} /></button>
         </div>
     );
 }
