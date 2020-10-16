b2079962518f970b6187dfd5d5053a7c7f2ec9e3
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-10-16 15:16:42 +0530

    clean up

--- frontend/app/src/Containers/textEditior.js
+++ frontend/app/src/Containers/textEditior.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import classes from './textEditor.module.css';
-import TextLines from '../Components/textLines';
 
 class TextEditor extends Component {
     state = {
@@ -9,30 +8,17 @@ class TextEditor extends Component {
     }
 
     componentDidMount() {
-        // console.log("component did mount");
-        window.onload = () => {
-            this.focusHandler();
-        }
+       
     }
 
     componentDidUpdate() {
-        // console.log("componentDidUpdate");
-    }
-
-    contentUpdateHandler = (event) => {
-        const x = event.target.value;
-        this.setState({ editorContent: x });
-    }
-
-    focusHandler = () => {
-        document.getElementById("tarea").focus();
+        
     }
 
     render() {
         return (
-            <div className={classes.Main} onClick={this.focusHandler}>
-                <textarea onChange={(event) => this.contentUpdateHandler(event)} id="tarea"></textarea>
-                <TextLines content={this.state.editorContent} />
+            <div className={classes.Main}>
+                
             </div>
         );
     }
