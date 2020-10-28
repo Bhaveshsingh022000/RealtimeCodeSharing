4ad66fa03b4557a7421af0314d3af0de825ee631
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-10-28 18:20:06 +0530

    added database connection

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+const MONGODB_URI = 'mongodb+srv://bhavesh_05:Bhavesh2017@cluster0-yuok1.mongodb.net/codeshare?retryWrites=true&w=majority';
 
 
-
-app.listen(3005);
+mongoose.connect(MONGODB_URI)
+    .then(res => {
+        console.log("Connected");
+        const server = app.listen(3005);
+        const io = require('./socket').init(server);
+        io.on('connection', socket =>{
+            console.log('Client connected');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
