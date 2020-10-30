bcda9c82d6ed300b97370c994fc60f6e3d9d0271
Bhavesh <bhaveshsingh022000@hotmail.com>
2020-10-30 20:21:06 +0530

    redux setup

--- frontend/app/src/store/reducers/Editor.js
+++ frontend/app/src/store/reducers/Editor.js
@@ -1,7 +1,8 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-    roomName: null
+    roomName: null,
+    error: false
 }
 
 const reducer = (state = initialState, action)=>{
@@ -11,6 +12,11 @@ const reducer = (state = initialState, action)=>{
                 ...state,
                 roomName: action.roomName
             };
+        case actionTypes.FETCHING_ROOM_FAILED:
+            return{
+                ...state,
+                error: true
+            }
         default:
             return state;
     }
